9c59e6aa402c3ef8f007a006e42ea285a4d0cf1f
ylya-greb <grebenukov.ylyaschool35@gmail.com>
2025-05-08 17:59:09 +0300

    homework5

---
+++ homework 5/B12.c
@@ -0,0 +1,48 @@
+/*
+ * B12.c
+ * 
+ * Copyright 2025 USER <USER@PC-1>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+unsigned int a,b,min,max;	
+scanf("%u",&a);	
+min=a%10;
+max=min;
+
+    	while(a)
+    	{
+		  b=a%10;
+		  if(b>max) 	
+		  {max=b;}
+		  if(min>b) 	
+		  {min=b;}
+			
+			a/=10;
+			}
+	
+	printf("%u %u",min,max);
+	return 0;
+}
+
---
+++ homework 5/B9.c
@@ -0,0 +1,59 @@
+/*
+ * B9.c
+ * 
+ * Copyright 2025 Ylya <Ylya@SADOVOI-PC>
+ * 
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+ * MA 02110-1301, USA.
+ * 
+ * 
+ */
+
+
+#include <stdio.h>
+#include <stdbool.h>
+
+
+
+int main(int argc, char **argv)
+{
+int a;
+bool flag=0;
+scanf("%d",&a);
+
+
+   while(a)
+   {
+	if((a%10)%2==0)
+	{;}
+	else
+	{flag=1;break;}
+	
+	a/=10;
+	
+	   
+	   }	
+
+if(!flag)
+{printf("YES\n");}
+else
+{printf("NO\n");}
+	
+
+	
+	
+	return 0;
+}
+
